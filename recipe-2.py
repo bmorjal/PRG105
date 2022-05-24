5aa913e98ef9b39583628a655997108b730be4b2
bmorjal <98046359+bmorjal@users.noreply.github.com>
2022-05-24 16:14:39 -0500

    Add files via upload

---
+++ recipe-2.py
@@ -0,0 +1,29 @@
+print('6 ingredient gooey swedish chocolate cake!')
+print('https://scrummylane.com/6-ingredient-gooey-swedish-chocolate-cake-kladdkaka/')
+print('')
+print('Ingredients')
+print('')
+print('1/2 cup of salted butter')
+print('1/2 cup of cocoa powder')
+print('1 and 3/4 cups sugar')
+print('1 cup plain flour')
+print('1 teaspoon vanilla extract')
+print('3 eggs')
+print('1 cake serves 8 to 10 people')
+print('')
+batchQuantity = float(input('Enter how many cakes you would like to make: '))
+print('')
+butter = 0.5 * batchQuantity #cup
+cocoa = 0.5 * batchQuantity #cup
+sugar = 1.75 * batchQuantity #cup
+flour = 1 * batchQuantity #cup
+vanilla = 1 * batchQuantity #teaspoon
+eggs = 3 * batchQuantity
+
+print(butter, '\t cups of salted butter')
+print(cocoa, '\t cup of cocoa powder')
+print(sugar, '\t cups sugar')
+print(flour, '\t cups plain flour')
+print(vanilla, '\t teaspoons vanilla extract')
+print(eggs, '\t eggs')
+
---
+++ recipe-main.py
@@ -0,0 +1,22 @@
+print('Easy sugar cookie recipe!')
+print('https://bellyfull.net/easy-sugar-cookie-recipe/')
+print('Ingredients')
+print('1 cup unsalted butter')
+print('12 tablespoons granulated sugar')
+print('2 cups of all purpose flour')
+print('1 teaspoon vanilla extract')
+print('Serving 34 cookies per batch')
+
+batchQuantity = float(input('How many batches would you like to make? '))
+
+butter = 1 * batchQuantity   #cups
+sugar = 12 * batchQuantity   #tablespoons
+vanilla = 1 * batchQuantity  #teaspoon
+flour = 2 * batchQuantity    #cups
+
+print('Ingredients')
+print(butter, "\t cups unsalted butter")
+print(sugar, "\t tablespoons granulated sugar")
+print(flour, "\t cups of all purpose flour")
+print(vanilla, "\t teaspoons vanilla extract")
+
